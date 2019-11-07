f1cd79533721d067371ef222d686505f8b1f93ff
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-07 16:41:51 +0100

    created two endpoints

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,4 +5,18 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.status(200).json({ message: 'GET request successful' });
+  next();
+});
+
+router.get('/:requestId', (req, res, next) => {
+  const { requestId } = req.params;
+  res.status(200).json({
+    message: 'GET a specific request successful',
+    id: requestId,
+  });
+  next();
+});
+
 export default router;
