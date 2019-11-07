b3273fcb2f0d4782c402c0acf041e7c78b9d1693
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-07 20:35:17 +0100

    Wrote tests for server invalid routes

---
+++ src/tests/api/server-test.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import dotenv from 'dotenv';
+
+import { server } from '../../../app';
+// ./node_modules/.bin/mocha --require @babel/register
+dotenv.config();
+chai.use(chaiHttp);
+chai.should();
+
+describe('GET /', () => {
+  it('should return 404, when an unavailable route is requested', (done) => {
+    chai.request(server).get('/api').end((req, res) => {
+      res.should.have.status(404);
+      res.should.be.a('object');
+      res.body.should.have.property('code').eql(404);
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('message').eql('Route unavailable on server.');
+      done();
+    });
+  });
+
+  it('should return 404, when an unavailable route is requested', (done) => {
+    chai.request(server).get('/api/vi/ok').end((req, res) => {
+      res.should.have.status(404);
+      res.should.be.a('object');
+      res.body.should.have.property('code').eql(404);
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('message').eql('Route unavailable on server.');
+      done();
+    });
+  });
+});
