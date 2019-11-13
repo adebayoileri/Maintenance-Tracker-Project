78fa93f12f7a8e08c64c2f61a5a759ef176e7ff2
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-13 08:18:49 +0100

    Pool connection

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,27 @@
 /* eslint-disable linebreak-style */
-import pg from 'pg';
+/* eslint-disable no-console */
+import { Pool, Client } from 'pg';
 
-pg.pool({
-
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+});
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+});
+client.connect();
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
 });
